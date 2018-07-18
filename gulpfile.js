44d02c7159c59563e6fece8835129e72f5149a55
OreyM <oreymgt@gmail.com>
2018-07-18 17:05:01 +0300

    js watch add

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,54 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify      = require('gulp-uglifyjs');
 
 var config = {
 
     path: {
-        sass: './develop/sass/**/*.sass',
-        html: './public/index.html'
+        sass:   './develop/sass/**/*.sass',
+        html:   './public/index.html',
+        js:     './develop/js/**/*.js'
     },
 
     output: {
-        cssName: 'main.min.css',
-        path: './public/css'
+        cssName:    'main.min.css',
+        cssPath:    './public/css',
+        jsName:     'common.min.js',
+        jsPath:     './public/js'
+    },
+
+    autoprefix: {
+        versions: ['last 5 versions']
     }
 }
 
-gulp.task('sass', function () {
+gulp.task('sass-task', function () {
     return gulp.src(config.path.sass)
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(concat(config.output.cssName))
         .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
+            browsers: config.autoprefix.versions,
             cascade: false
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(config.output.path))
+        .pipe(gulp.dest(config.output.cssPath))
         .pipe(browserSync.stream());
 });
 
-gulp.task('server', function () {
+gulp.task('scripts-task', function() {
+    return gulp.src(config.path.js)
+        .pipe(concat(config.output.jsName))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.output.jsPath))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('server-task', function () {
 
    browserSync.init({
 
@@ -40,14 +56,15 @@ gulp.task('server', function () {
           //for the anybody files
           baseDir: "public/",
           directory: true
-          //for the current file
+          // for the current file
           // baseDir: "public/",
           // directory: true
       }
    });
 
-   gulp.watch(config.path.sass, ['sass']);
+   gulp.watch(config.path.sass, ['sass-task']);
+   gulp.watch(config.path.js, ['scripts-task']);
    gulp.watch(config.path.html).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'server']);
+gulp.task('default', ['sass-task', 'scripts-task', 'server-task']);
